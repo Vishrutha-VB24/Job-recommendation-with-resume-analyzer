735bb5d3e24f889f774e64fcb43ceb58073dd486
Vishrutha V B <Vishruthavb24@gmail.com>
2024-11-06 15:35:11 +0530

    uploadform and small update

--- frontend/src/routes/Upload.tsx
+++ frontend/src/routes/Upload.tsx
@@ -1,11 +1,12 @@
 import UploadForm from '@/components/UploadForm'
 import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute('/upload')({
+export const Route = createFileRoute('/Upload')({
   component: upload,
 })
 
 function upload() {
+  
   return (
     <div className="flex justify-center  items-center h-screen">
       <UploadForm></UploadForm>
